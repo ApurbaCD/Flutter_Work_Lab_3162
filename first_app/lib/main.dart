45d17a3a7ac6bfb2a09f9efeee1fdbea25cbe147
ApurbaCD <apurbadey31273127@gmail.com>
2024-05-22 18:58:06 +0600

    save function added

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 void main(){
   runApp(const MyApp());
@@ -26,6 +27,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // text controller 
+  final  _controller = TextEditingController();
   //list of todo tasks
   List toDoList=[
     ['make tutorial',false],
@@ -37,14 +40,27 @@ class _HomePageState extends State<HomePage> {
       toDoList[index][1]=!toDoList[index][1];
     });
   }
+  // save new task 
+  void saveNewTask(){
+    setState(() {
+      toDoList.add([_controller.text,false]);
+       _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
   void createNewTask(){
     showDialog(
       context: context,
       builder: (context){
-        return DialogBox();
+        return DialogBox(
+          controller: _controller ,
+          onSave: saveNewTask,
+          onCancel: () => Navigator.of(context).pop(),
+        );
       },
       );
   }
+  
 
   @override
   Widget build(BuildContext context) {
@@ -65,6 +81,7 @@ class _HomePageState extends State<HomePage> {
           return ToDoList(taskName: toDoList[index][0], 
           taskCompleted: toDoList[index][1], 
           onChange: (value)=>checkboxchanged(value,index),
+          deleteFunction: (context)=> deleteTask(),
           );
         },
       )
@@ -76,44 +93,61 @@ class ToDoList extends StatelessWidget {
   final String taskName;
   final bool taskCompleted;
   Function(bool?)? onChange;
+  Function(BuildContext)? deleteFunction;
 
   ToDoList({
     super.key,
     required this.taskName,
     required this.taskCompleted,
     required this.onChange,
+    required this.deleteFunction,
     });
   
   Widget build(BuildContext context){
     return Padding(
       padding: const EdgeInsets.only(left: 25.0,right: 25.0,top: 25.0),
-      child: Container(
-        padding: EdgeInsets.all(24),
-        decoration: BoxDecoration(
-          color: Colors.yellow,
-          borderRadius: BorderRadius.circular(12)
-          ),
-        child: Row(
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
           children: [
-            //checkbox 
-            Checkbox(
-              value: taskCompleted,
-              onChanged: onChange,
-              activeColor: Colors.black,
-              ),
-            //task name
-            Text(
-              taskName,
-              style: TextStyle(decoration:  taskCompleted? TextDecoration.lineThrough :TextDecoration.none),
-              ),
+            SlidableAction(
+              onPressed: deleteFunction,
+              icon:Icons.delete,
+              backgroundColor: Colors.red.shade300,
+              )
           ],
+        ),
+        child: Container(
+          padding: EdgeInsets.all(24),
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+            borderRadius: BorderRadius.circular(12)
+            ),
+          child: Row(
+            children: [
+              //checkbox 
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChange,
+                activeColor: Colors.black,
+                ),
+              //task name
+              Text(
+                taskName,
+                style: TextStyle(decoration:  taskCompleted? TextDecoration.lineThrough :TextDecoration.none),
+                ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
  class DialogBox extends StatelessWidget{
-  const DialogBox({super.key});
+  final controller;
+  VoidCallback onSave;
+  VoidCallback onCancel;
+  DialogBox({super.key,required this.controller,required this.onSave,required this.onCancel});
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
@@ -121,14 +155,22 @@ class ToDoList extends StatelessWidget {
       content:Container(
         height: 120,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             TextField(
-              decoration: InputDecoration(
+                controller: controller,
+                decoration: InputDecoration(
                 border:OutlineInputBorder(),
                 hintText: 'Add a new task',
                 ),
             ),
-            Row(children: [
+            //button -> save + cancle
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+              MyButton(text: 'save', onPressed:onSave),
+              const SizedBox(width: 8),
+              MyButton(text: 'cancel', onPressed:onCancel),
 
             ],)
           ],
@@ -148,6 +190,10 @@ class MyButton extends StatelessWidget{
     });
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialButton(
+      onPressed: onPressed,
+      color: Theme.of(context).primaryColor,
+      child: Text(text),
+    );
   }
 }

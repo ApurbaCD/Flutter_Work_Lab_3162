dca1a7bacf9362b101fd0621d9d373c372094291
ApurbaCD <apurbadey31273127@gmail.com>
2024-05-30 15:15:38 +0600

    add language localization not using the depandencies

---
+++ first_app/lib/All_string.dart
@@ -0,0 +1,45 @@
+// import 'package:flutter/material.dart';
+
+// class Page extends StatefulWidget {
+//   const Page({super.key});
+
+//   @override
+//   _PageState createState() => _PageState();
+// }
+
+// class _PageState extends State<Page> {
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var list1=[];
+//     var list2=[];
+//     list1.add('todo_list');
+//     list2.add('সবকিছুর তালিকা');
+//     list1.add('add a new task ');
+//     list2.add('একটি নতুন টাস্ক যোগ করুন');
+//     list1.add('save');
+//     list2.add('সংরক্ষণ');
+//     list1.add('delete');
+//     list2.add('মুছে ফেলা');
+
+
+//     return Scaffold(
+
+//     );
+//   }
+// }
+
+class Other{
+  static final  list1=[
+    'সবকিছুর তালিকা',
+    'একটি নতুন টাস্ক যোগ করুন',
+    'সংরক্ষণ',
+    'মুছে ফেলা',
+  ];
+   static final list2=[
+    'todo_list',
+    'add a new task ',
+    'save',
+    'delete',
+   ];
+}
--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,31 +1,237 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/widgets.dart';
+import 'package:first_app/All_string.dart';
+// import 'package:flutter_slidable/flutter_slidable.dart';
+List s=Other.list1;
 void main(){
-      runApp(const MyApp());
-
+  runApp(const MyApp());
 }
-
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    const String appTitle = 'Flutter layout demo';
     return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(appTitle),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              Expanded(child:Text('Hello World'),),
-              Expanded(child:Text('i am aprbadey '),),
-            ],
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
     );
   }
 }
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+class _HomePageState extends State<HomePage> {
+  //  List s=Other.list1;
+  // text controller 
+  final  _controller = TextEditingController();
+  //list of todo tasks
+  List toDoList=[
+    ['make tutorial',false],
+    ['do exercise',false],
+  ];
+  //checkbox was tapped 
+  void checkboxchanged( bool? value , index){
+    setState(() {
+      toDoList[index][1]=!toDoList[index][1];
+    });
+  }
+  // save new task 
+  void saveNewTask(){
+    setState(() {
+      toDoList.add([_controller.text,false]);
+       _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
+  void createNewTask(){
+    showDialog(
+      context: context,
+      builder: (context){
+        return DialogBox(
+          controller: _controller ,
+          onSave: saveNewTask,
+          onCancel: () => Navigator.of(context).pop(),
+        );
+      },
+      );
+  }
+  void deleteTask(int index){
+    setState(() {
+      toDoList.removeAt(index);
+    });
+  }
+
+  void bangla_changer(){
+    setState(() {
+      s=Other.list1;
+    });
+    
+  }
+  void english_changer(){
+    setState(() {
+     s=Other.list2;
+    });
+   
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( 
+      backgroundColor: Color.fromARGB(255, 123, 177, 179), 
+      floatingActionButton:Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          TextButton(onPressed: bangla_changer, child: Text('Bangla')),
+          TextButton(onPressed: english_changer, child: Text('English')), 
+          FloatingActionButton(
+            onPressed:createNewTask,
+            child: Icon(
+              Icons.add
+              ),
+          ),
+        ],
+      ), 
+       appBar: AppBar(
+        title: Text(s[0]),
+        elevation: 0,
+      ),
+      body:ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder:(context, index){
+          return ToDoList(taskName: toDoList[index][0], 
+          taskCompleted: toDoList[index][1], 
+          onChange: (value)=>checkboxchanged(value,index),
+          deleteFunction: (context)=> deleteTask(index),
+          );
+        },
+      )
+    );
+  }
+}
+class ToDoList extends StatelessWidget {
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChange;
+  Function(BuildContext)? deleteFunction;
+  ToDoList({
+    super.key,
+    required this.taskName,data,
+    required this.taskCompleted,
+    required this.onChange,
+    required this.deleteFunction,
+    });
+  
+  Widget build(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0,right: 25.0,top: 25.0),
+      // child: Slidable(
+      //   endActionPane: ActionPane(
+      //     motion: StretchMotion(),
+      //     children: [
+      //       SlidableAction(
+      //         onPressed: deleteFunction,
+      //         icon:Icons.delete,
+      //         backgroundColor: Colors.red.shade300,
+      //         borderRadius: BorderRadius.circular(12),
+      //         )
+      //     ],
+      //   ),
+        child: Container(
+          padding: EdgeInsets.all(24),
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 248, 248, 248),
+            borderRadius: BorderRadius.circular(12)
+            ),
+          child: Row(
+            children: [
+              //checkbox 
+              Checkbox(
+                value: taskCompleted,
+                onChanged: onChange,
+                activeColor: Colors.black,
+                ),
+              //task name
+              Text(
+                taskName,
+                style: TextStyle(fontSize: 20,decoration:  taskCompleted? TextDecoration.lineThrough :TextDecoration.none),
+                ),
+            ],
+          ),
+        ),
+      //),
+    );
+  }
+}
+ class DialogBox extends StatelessWidget{
+  final controller;
+  VoidCallback onSave;
+  VoidCallback onCancel;
+  DialogBox({super.key,required this.controller,required this.onSave,required this.onCancel});
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.yellow[300],
+      content:Container(
+        height: 120,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TextField(
+                controller: controller,
+                decoration: InputDecoration(
+                border:OutlineInputBorder(),
+                hintText:s[1],
+                ),
+            ),
+            //button -> save + cancl
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+              MyButton(text: s[2], onPressed:onSave),
+              const SizedBox(width: 8),
+              MyButton(text: s[3], onPressed:onCancel),
+            ],)
+          ],
+        ),
+        ),
+    );
+  }
+ }
+class MyButton extends StatelessWidget{
+  final String text;
+  VoidCallback onPressed;
+  MyButton({
+    super.key, 
+    required this.text, 
+    required this.onPressed
+    });
+  @override
+  Widget build(BuildContext context) {
+    return MaterialButton(
+      onPressed: onPressed,
+      color: Theme.of(context).primaryColor,
+      child: Text(text),
+    );
+  }
+}
+
+// class Language extends StatelessWidget{
+//   void bangla_changer(){
+//     print("ok");
+//   }
+//   void english_changer(){
+//     print("ok");
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return  Row(
+//       children: [
+//           TextButton(onPressed: bangla_changer, child: Text('Bangla')),
+//           TextButton(onPressed: english_changer, child: Text('English')),          
+//       ],
+//     );
+//   }
+// }
